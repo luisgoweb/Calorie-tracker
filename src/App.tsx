bb45687f62bfabc2759a9a48d661759202fa5cc6
luisgoweb <luismga2012@gmail.com>
2025-06-12 21:53:15 -0400

    App.tsx: Se agrega header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl">Calorie Tracker</h1>
+        <header className="bg-lime-600 py-3">
+            <div className="max-w-4xl mx-auto flex justify-between">
+                <h1 className="text-center text-lg font-bold text-white uppercase">Contador de Calorias</h1>
+            </div>
+            
+        </header>
+      
     </>
   )
 }
