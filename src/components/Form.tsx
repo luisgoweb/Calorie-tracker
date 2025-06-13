70792c5b51dfa664495cb3567d59b5a8ff84e031
luisgoweb <luismga2012@gmail.com>
2025-06-12 22:34:32 -0400

    Form.tsx: Formulario en desarrollo, trabajando en el select

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,9 +2,17 @@
 
 const Form = () => {
   return (
-    <>
-        <h2>Formulario</h2>
-    </>
+    <form className="space-y-5 p-10 shadow bg-white rounded-lg">
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="category">Categoría:</label>
+            <select 
+            className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+            id="category"
+            >
+
+            </select>
+        </div>
+    </form>
   )
 }
 
