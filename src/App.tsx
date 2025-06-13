3877b047cc1108e3102632dacadd7fcaa032020a
luisgoweb <luismga2012@gmail.com>
2025-06-12 22:08:35 -0400

    Se crea carpeta components y dentro componente Form.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Form from "./components/Form"
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
 
         <section className="bg-lime-500 py-20 px-5">
             <div className="max-w-4xl mx-auto">
-                <p>Formulario</p>
+                <Form />
             </div>
         </section>
       
---
+++ src/components/Form.tsx
@@ -0,0 +1,11 @@
+
+
+const Form = () => {
+  return (
+    <>
+        <h2>Formulario</h2>
+    </>
+  )
+}
+
+export default Form
