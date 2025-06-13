2eb066f5f80a63388b2579df3c79bf87bf9235c3
luisgoweb <luismga2012@gmail.com>
2025-06-12 22:06:53 -0400

    App.tsx: Se crea section para el formulario

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,13 @@ function App() {
             <div className="max-w-4xl mx-auto flex justify-between">
                 <h1 className="text-center text-lg font-bold text-white uppercase">Contador de Calorias</h1>
             </div>
-            
         </header>
+
+        <section className="bg-lime-500 py-20 px-5">
+            <div className="max-w-4xl mx-auto">
+                <p>Formulario</p>
+            </div>
+        </section>
       
     </>
   )
