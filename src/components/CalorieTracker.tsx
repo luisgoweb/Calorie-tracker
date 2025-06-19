3d876b164774060a9436c1a359ff5a50921ca850
luisgoweb <luismga2012@gmail.com>
2025-06-18 20:03:32 -0400

    Se crea estadistica para mostrar las colorías consumidas y quemadas

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import type { Activity } from "../types"
 
 
@@ -6,10 +7,25 @@ type CalorieTrackerProps = {
 }
 
 const CalorieTracker = ({activities}: CalorieTrackerProps) => {
+
+    const calorieConsumed = useMemo(()=> activities.reduce((total,activity) => activity.category === 1 ? total + +activity.calories : total, 0) ,[activities])
+    const calorieBurn = useMemo(()=> activities.reduce((total,activity) => activity.category === 2 ? total + +activity.calories : total, 0) ,[activities])
   return (
-    <div>
-      
-    </div>
+    <>
+        <h2 className="text-4xl text-white text-center font-black">Resumen de Calorías</h2> 
+        <div className="flex flex-col items-center md:flex-row  md:justify-between gap-5 mt-10">
+            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center"> 
+                <span className="font-black text-6xl text-orange">{calorieConsumed}</span>
+                Cosumidas
+            </p>
+
+            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center"> 
+                <span className="font-black text-6xl text-orange">{calorieBurn}</span>
+                Ejercicio
+            </p>
+        </div>
+       
+    </>
   )
 }
 
